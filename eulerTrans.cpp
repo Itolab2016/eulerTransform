59b20894398df83c2e7630c1006a355490753fa3
Kouhei <kouhei.ito@itolab-ktc.com>
2016-07-09 11:04:10 +0900

    つくりかけ
    順変換部分ができました。

--- eulerTrans.cpp
+++ eulerTrans.cpp
@@ -1,3 +1,4 @@
+//C Kouhei Ito
 #include <stdio.h>
 #include <math.h>
 
@@ -24,10 +25,19 @@ int main()
 
 short eulerTr(double* x, double* y, double* z, double phi, double theta, double psi)
 {
-  
+ double X, Y, Z;
+ 
+ X=cos(theta)*cos(psi)*(*x) + cos(theta)*sin(psi)*(*y) - sin(theta)*(*z);
+ Y=(sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi))*(*x) + (sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi))*(*y) + sin(phi)*cos(theta)*(*z);
+ Z=(cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi))*(*x) + (cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi))*(*y) + cos(phi)*cos(theta)*(*z);
+ *x=X;
+ *y=Y;
+ *z=Z;
 }
 
 short eulerinvTr(double* x, double* y, double* z, double phi, double theta, double psi)
 {
-
+ *x=0.0;
+ *y=0.0;
+ *z=0.0;
 }
