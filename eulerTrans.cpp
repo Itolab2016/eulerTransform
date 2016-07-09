a6e2d0fcf6ef3f109622010e3786a4adc32eb7d5
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-07-09 13:04:57 +0900

    座標変換サンプルプログラム完成
    現在のところ、順変換、逆変換で、最初に与えた世界座標が元に戻ることが確認できるバージョンが完成した。
    間違いが無いかどうかは、まだ確認が足りないような気がする。
    このプログラムが、正しく動いているのか確認することを考えてもらいたい。

--- eulerTrans.cpp
+++ eulerTrans.cpp
@@ -1,8 +1,8 @@
-//C Kouhei Ito
+//(C) Kouhei Ito, Kanazawa Tecnical College JAPAN
 #include <stdio.h>
 #include <math.h>
 
-//以下の関数は変換したい座標をポインタ（変数のアドレス）で渡します。
+//以下で宣言される関数は変換したい座標をポインタ（変数のアドレス）で渡します。
 //変換した結果は渡した変数を上書きした形で返されます。
 short eulerTr(double* x, double* y, double* z, double phi, double theta, double psi);//回転の座標変換関数の宣言
 short eulerinvTr(double* x, double* y, double* z, double phi, double theta, double psi);//逆回転の座標変換関数の宣言
@@ -16,18 +16,24 @@ int main()
  y=2.0;
  z=3.0;
  
- phi   = 60.0 * M_PI/180.0;//sin()等は扱う単位がラジアンなので、解りやすい度からラジアンに変換
+ phi   = 60.0 * M_PI/180.0;             //sin()等は扱う単位がラジアンなので、解りやすい度からラジアンに変換
  theta = 20.0 * M_PI/180.0;
  psi   = 15.0 * M_PI/180.0;
  
- eulerTr(&x, &y, &z, phi, theta, psi);//&を変数の前につけると変数のアドレスを渡すことができる
- printf("(%f,%f,%f)\n",x,y,z);//変換結果表示
+ printf("世界（地球）座標系での座標\n");
+ printf("(%f,%f,%f)\n",x,y,z);          //変換前の座標表示
+ 
+ eulerTr(&x, &y, &z, phi, theta, psi);  //&を変数の前につけると変数のアドレスを渡すことができる
+ printf("世界座標系-->ロボット座標系への座標変換結果\n");
+ printf("(%f,%f,%f)\n",x,y,z);          //変換結果表示
  
  eulerinvTr(&x, &y, &z,phi, theta, psi);
+ printf("ロボット座標系-->世界座標への座標変換結果（元に戻す）\n");
  printf("(%f,%f,%f)\n",x,y,z);
  
 }
 
+//ここから関数の定義
 short eulerTr(double* x, double* y, double* z, double phi, double theta, double psi)
 {
  double X, Y, Z;
@@ -43,14 +49,11 @@ short eulerTr(double* x, double* y, double* z, double phi, double theta, double 
 
 short eulerinvTr(double* x, double* y, double* z, double phi, double theta, double psi)
 {
-
  double X, Y, Z;
  
- X=cos(theta)*cos(psi)*(*x)                   + (sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi))*(*y) - (cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi))*(*z);
-
-
- Y=cos(theta)*sin(psi)*(*x)                  + (sin(phi)*sin(theta)*sin(psi) + cos(phi)*cos(psi))*(*y) + (cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi))*(*z);
- Z=sin(theta)*(*x)                            + sin(phi)*cos(theta)*(*y)                                + cos(phi)*cos(theta)*(*z);
+ X= cos(theta)*cos(psi)*(*x) + (sin(phi)*sin(theta)*cos(psi) - cos(phi)*sin(psi))*(*y) + (cos(phi)*sin(theta)*cos(psi) + sin(phi)*sin(psi))*(*z);
+ Y= cos(theta)*sin(psi)*(*x) + (sin(phi)*sin(theta)*sin(psi) + cos(phi)*cos(psi))*(*y) + (cos(phi)*sin(theta)*sin(psi) - sin(phi)*cos(psi))*(*z);
+ Z=-sin(theta)*(*x)          + sin(phi)*cos(theta)*(*y)                                + cos(phi)*cos(theta)*(*z);
  *x=X;
  *y=Y;
  *z=Z;
